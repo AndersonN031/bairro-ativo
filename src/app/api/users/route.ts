ab4e291b1ccb3eddca92b17a36ec7049aad4a25a
AndersonN031 <devandersonnunes078@gmail.com>
2024-09-03 08:35:20 -0300

    tester

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,36 +1,36 @@
 import { db } from "@/src/lib/db"
 import { NextRequest, NextResponse } from "next/server"
 
-export async function GET() {
-    try {
-        const usuarios = await db.usuario.findMany();
-        return NextResponse.json(usuarios)
-    } catch (error) {
-        return NextResponse.json({
-            message: "Error ao buscar usuários."
-        })
-    }
-}
+// export async function GET() {
+//     try {
+//         const usuarios = await db.usuario.findMany();
+//         return NextResponse.json(usuarios)
+//     } catch (error) {
+//         return NextResponse.json({
+//             message: "Error ao buscar usuários."
+//         })
+//     }
+// }
 
-export async function POST(req: NextRequest) {
-    const { nome, email, senha_hash, papel_id } = await req.json();
-    try {
-        const user = await db.usuario.create({
-            data: {
-                nome,
-                email,
-                senha_hash,
-                papel_id,
-            }
-        });
-        return NextResponse.json({ message: "Usuário criado com sucesso!", user })
+// export async function POST(req: NextRequest) {
+//     const { nome, email, senha_hash, papel_id } = await req.json();
+//     try {
+//         const user = await db.usuario.create({
+//             data: {
+//                 nome,
+//                 email,
+//                 senha_hash,
+//                 papel_id,
+//             }
+//         });
+//         return NextResponse.json({ message: "Usuário criado com sucesso!", user })
 
-    } catch (error) {
-        return NextResponse.json({
-            message: "Error creating user",
-            error,
-        }, {
-            status: 500,
-        });
-    }
-}
+//     } catch (error) {
+//         return NextResponse.json({
+//             message: "Error creating user",
+//             error,
+//         }, {
+//             status: 500,
+//         });
+//     }
+// }
