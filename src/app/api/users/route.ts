cc6743e920dc24097fc344dcfd3c6e6ba10c84ea
AndersonN031 <devandersonnunes078@gmail.com>
2024-09-03 08:30:04 -0300

    modificando a chamada do prisma no route.ts

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,11 +1,14 @@
 
 import { NextRequest, NextResponse } from "next/server"
-import { db } from "@/src/lib/db";
+// import { db } from "@/src/lib/db";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 export async function GET() {
     try {
-        const usuarios = await db.usuario.findMany();
-        return NextResponse.json(usuarios)
+        const user = await prisma.usuario.findMany();
+        return NextResponse.json(user)
     } catch (error) {
         return NextResponse.json({
             message: "Error ao buscar usuários."
@@ -13,25 +16,25 @@ export async function GET() {
     }
 }
 
-export async function POST(req: NextRequest) {
-    const { nome, email, senha_hash, papel_id } = await req.json();
-    try {
-        const user = await db.usuario.create({
-            data: {
-                nome,
-                email,
-                senha_hash,
-                papel_id,
-            }
-        });
-        return NextResponse.json({ message: "Usuário criado com sucesso!", user })
+// export async function POST(req: NextRequest) {
+//     const { nome, email, senha_hash, papel_id } = await req.json();
+//     try {
+//         const user = await db.usuario.create({
+//             data: {
+//                 nome,
+//                 email,
+//                 senha_hash,
+//                 papel_id,
+//             }
+//         });
+//         return NextResponse.json({ message: "Usuário criado com sucesso!", user })
 
-    } catch (error) {
-        return NextResponse.json({
-            message: "Error creating user",
-            error,
-        }, {
-            status: 500,
-        });
-    }
-}
+//     } catch (error) {
+//         return NextResponse.json({
+//             message: "Error creating user",
+//             error,
+//         }, {
+//             status: 500,
+//         });
+//     }
+// }
