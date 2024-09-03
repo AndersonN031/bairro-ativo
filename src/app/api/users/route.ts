546bc8e194c0f85c9b72db83139e783d0ff610af
AndersonN031 <devandersonnunes078@gmail.com>
2024-09-03 08:33:05 -0300

    tester

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,25 +1,21 @@
-
+import { db } from "@/src/lib/db"
 import { NextRequest, NextResponse } from "next/server"
-// import { db } from "@/src/lib/db";
-import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
-
-// export async function GET() {
-//     try {
-//         const user = await prisma.usuario.findMany();
-//         return NextResponse.json(user)
-//     } catch (error) {
-//         return NextResponse.json({
-//             message: "Error ao buscar usuários."
-//         })
-//     }
-// }
+export async function GET() {
+    try {
+        const usuarios = await db.usuario.findMany();
+        return NextResponse.json(usuarios)
+    } catch (error) {
+        return NextResponse.json({
+            message: "Error ao buscar usuários."
+        })
+    }
+}
 
 export async function POST(req: NextRequest) {
     const { nome, email, senha_hash, papel_id } = await req.json();
     try {
-        const user = await prisma.usuario.create({
+        const user = await db.usuario.create({
             data: {
                 nome,
                 email,
