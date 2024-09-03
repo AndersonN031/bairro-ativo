3d924a2489fd9a471b3fdfb6464f2e40534f5f59
AndersonN031 <devandersonnunes078@gmail.com>
2024-09-03 08:31:18 -0300

    modificando a chamada do prisma no route.ts

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -5,36 +5,36 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export async function GET() {
-    try {
-        const user = await prisma.usuario.findMany();
-        return NextResponse.json(user)
-    } catch (error) {
-        return NextResponse.json({
-            message: "Error ao buscar usuários."
-        })
-    }
-}
-
-// export async function POST(req: NextRequest) {
-//     const { nome, email, senha_hash, papel_id } = await req.json();
+// export async function GET() {
 //     try {
-//         const user = await db.usuario.create({
-//             data: {
-//                 nome,
-//                 email,
-//                 senha_hash,
-//                 papel_id,
-//             }
-//         });
-//         return NextResponse.json({ message: "Usuário criado com sucesso!", user })
-
+//         const user = await prisma.usuario.findMany();
+//         return NextResponse.json(user)
 //     } catch (error) {
 //         return NextResponse.json({
-//             message: "Error creating user",
-//             error,
-//         }, {
-//             status: 500,
-//         });
+//             message: "Error ao buscar usuários."
+//         })
 //     }
 // }
+
+export async function POST(req: NextRequest) {
+    const { nome, email, senha_hash, papel_id } = await req.json();
+    try {
+        const user = await prisma.usuario.create({
+            data: {
+                nome,
+                email,
+                senha_hash,
+                papel_id,
+            }
+        });
+        return NextResponse.json({ message: "Usuário criado com sucesso!", user })
+
+    } catch (error) {
+        return NextResponse.json({
+            message: "Error creating user",
+            error,
+        }, {
+            status: 500,
+        });
+    }
+}
