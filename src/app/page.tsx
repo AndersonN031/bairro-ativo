7b95e108850ab6fd4e0baa056d838e2bba3bf7ee
AndersonN031 <devandersonnunes078@gmail.com>
2024-09-06 07:57:53 -0300

    redirecionando rota raiz

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,26 @@
-'use client';
+// 'use client';
 
-import SideBar from '../components/sideBarComponente';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '../lib/auth';
+import Sidebar from '../components/sideBarComponente';
+import { redirect } from 'next/navigation';
 
-export default function Home() {
+
+export default async function Home() {
+
+  const session = await getServerSession(authOptions);
+
+  if (!session?.user) {
+    redirect('/login')
+  }
 
   return (
-    <SideBar />
+    <div>
+      <Sidebar />
+      <h1>Dashboard</h1>
+      <p>Por favor faça login para acessar o Dashboard do Administrador! </p>
+
+    </div>
   );
+
 }
